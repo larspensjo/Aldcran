a2d92550b1614bcd305efa2ffe7b9fee95ab7d95
Lars Pensjo <lars.pensjo@gmail.com>
2014-08-30 11:04:29 +0200

    First files

---
+++ aldcran.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com/larspensjo/go-monotonic-graycode"
+)
+
+func main() {
+	// monotonic_gray_code.Init(16)
+	fmt.Println("Hello, world", monotonic_gray_code.Pi(10))
+}
