d0e092bb65265e09b08f061e196dd1967da9d2be
Lars Pensjo <lars.pensjo@gmail.com>
2014-09-27 11:38:45 +0200

    Export merge functionality

---
+++ MergePrograms/merge_export.go
@@ -0,0 +1,27 @@
+// Copyright 2014 Lars Pensjö
+//
+// This file is part of Aldcran.
+//
+// Aldcran is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, version 3.
+//
+// Aldcran is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Aldcran.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+// Based on http://www.xmailserver.org/diff2.pdf
+package merge
+
+// Do a random merge transform on two programs, and return the new program
+func RandomMerge(p1, p2 []byte) []byte {
+	graph := findMatchPoints(p1, p2)
+	_, p := findShortestPathButeForce(graph, 0, 0)
+	newProg := p.randomMerge(p1, p2)
+	return newProg
+}
